4913738af9ac78f3c9970bbc7f2ed0582a64bb3c
tao.zhou <tao.zhou@genlot.com>
2022-04-15 15:34:44 +0800

    Site updated: 2022-04-15 15:34:41

--- js/memory.js
+++ js/memory.js
@@ -1,3 +1,19 @@
+function moreDays() {
+  const days = [
+    "2021-07-31 初识",
+    "2021-10-03 ▇▇",
+    "2022-01-29 在一起",
+    "2022-02-03 ▇▇",
+    "2022-04-05 ▇▇",
+    ];
+  var html = "";
+  days.forEach((day, index) => {
+    html += "<li>" + day + "</li>";
+  })
+  document.getElementsByClassName('moreDays')[0].innerHTML = html;
+}
+moreDays();
+
 function Since(date, desc) {
   let miliSecond,totalDays;
   let tmpDate = Date.parse(date);
@@ -14,10 +30,14 @@ function Since(date, desc) {
   return totalDays;
 }
 
-var relationship = '2022-01-29'
+var relationship = '2021-07-31'
 document.getElementsByClassName('c1')[0].innerText = Since(relationship, false);
 document.getElementsByClassName('b3')[0].innerText = 'Since ' + relationship;
 
+var relationship1 = '2022-01-29'
+document.getElementsByClassName('c1')[1].innerText = Since(relationship1, false);
+document.getElementsByClassName('b3')[1].innerText = 'Since ' + relationship1;
+
 var relationship2 = '2022-07-20'
-document.getElementsByClassName('c1')[1].innerText = Since(relationship2, true);
-document.getElementsByClassName('b3')[1].innerText = 'The day is ' + relationship2;
+document.getElementsByClassName('c1')[2].innerText = Since(relationship2, true);
+document.getElementsByClassName('b3')[2].innerText = 'The day is ' + relationship2;
